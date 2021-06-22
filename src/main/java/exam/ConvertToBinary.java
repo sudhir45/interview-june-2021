f631400ea71d9c02ed4277cd5731ced01c84fc7b
Sudhir Dubey <46814039+sudhir45@users.noreply.github.com>
2021-06-22 12:15:03 +0530

    Implemented decimal to binary r1

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,18 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    if(n<0){
+      System.out.println("Negative values not supported yet.")
+    }
+    else{
+     int rem=0;
+     string  output_num="";
+     while(n!=0){
+      rem = n%2;
+      n = n/2;
+      output_num = rem+output_num
+     }   
+    }
+    return ouputnum;
   }
 }
